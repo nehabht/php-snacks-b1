f0c5b994690595614da5c24bb5e60633dc129644
nehabht <ne.bhatti2@icloud.com>
2022-05-20 12:51:42 +0200

    initial commit

---
+++ snack1/index.php
@@ -0,0 +1,44 @@
+<!-- 
+    ## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, 
+punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+ -->
+
+<?php
+
+$partiteMilano = [
+
+    "partita1" => "Milano - Cantù",
+    "partita2" => "Milano - Biella",
+    "partita3" => "Milano - Brescia",
+
+
+];
+
+$risultati = [
+    [
+        "squadraCasa" => "Milano",
+        "squadraOspite" => "Cantù",
+        "punteggioCasa" => 55,
+        "punteggioOspite" => 60,
+    ],
+    [
+        "squadraCasa" => "Milano",
+        "squadraOspite" => "Biella",
+        "punteggioCasa" => 62,
+        "punteggioOspite" => 77,
+    ],
+    [
+        "squadraCasa" => "Milano",
+        "squadraOspite" => "Brescia",
+        "punteggioCasa" => 65,
+        "punteggioOspite" => 66,
+    ],
+
+];
+
+
+?>
